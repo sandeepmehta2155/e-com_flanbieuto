e6fdfbcf1934253dc0b22b4007aa2effeb92b6d9
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2021-08-27 01:02:44 +0530

    book batches modified

--- src/HomePage-Components/book-batch.js
+++ src/HomePage-Components/book-batch.js
@@ -16,17 +16,6 @@ export function BookBatches() {
           />
         )
       )}
-      {/* {img.length !== 0 ? (
-        img.map((obj) => (
-          <img className="bookBatchPills" src={obj} alt="loading" key={obj} />
-        ))
-      ) : (
-        <img
-          className="bookBatchPills"
-          src="https://as1.ftcdn.net/jpg/02/01/18/68/500_F_201186834_NLRQImmmfLmOHEQpQPXB6wh4F4quHloV.jpg"
-          alt="loading"
-        />
-      )} */}
     </div>
   );
 }
