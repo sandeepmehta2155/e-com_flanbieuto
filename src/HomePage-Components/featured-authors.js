38cfcd188e52b8b4b0afe0f7fdb3ab923f297483
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2021-08-27 01:11:03 +0530

    loading img removed

--- src/HomePage-Components/featured-authors.js
+++ src/HomePage-Components/featured-authors.js
@@ -7,58 +7,37 @@ export function FeaturedAuthors() {
     <>
       <div className="featuredObjects">
         <h1> Featured India Authors </h1>
+        {FeaturedAuthors.length === 0 && <h1> Loading... </h1>}
         <ul className="img-group">
-          {FeaturedAuthors.map((obj) => {
-            return FeaturedAuthors.length !== 0 ? (
+          {FeaturedAuthors.map(
+            (obj) =>
               obj.nation === "India" && (
                 <li key={obj.icon} className="list list-group-item">
                   <img className="circle" alt="loading" src={obj.icon} />
                 </li>
               )
-            ) : (
-              <img
-                className="bookBatchPills"
-                src="https://as1.ftcdn.net/jpg/02/01/18/68/500_F_201186834_NLRQImmmfLmOHEQpQPXB6wh4F4quHloV.jpg"
-                alt="loading"
-              />
-            );
-          })}
+          )}
         </ul>
         <h1> Featured International Authors </h1>
+        {FeaturedAuthors.length === 0 && <h1> Loading... </h1>}
         <ul className="img-group">
-          {FeaturedAuthors.map((obj) => {
-            return FeaturedAuthors.length !== 0 ? (
+          {FeaturedAuthors.map(
+            (obj) =>
               obj.nation !== "India" && (
                 <li key={obj.icon} className="list list-group-item">
                   <img className="circle" alt="loading" src={obj.icon} />
                 </li>
               )
-            ) : (
-              <img
-                className="bookBatchPills"
-                src="https://as1.ftcdn.net/jpg/02/01/18/68/500_F_201186834_NLRQImmmfLmOHEQpQPXB6wh4F4quHloV.jpg"
-                alt="loading"
-              />
-            );
-          })}
+          )}
         </ul>
         <h1> Featured Series</h1>
+        {FeaturedSeries.length === 0 && <h1> Loading... </h1>}
         <ul className="img-group">
-          {FeaturedSeries ? (
-            FeaturedSeries.map((obj) => {
-              return (
-                <li key={obj.titleSrc} className="list list-group-item">
-                  <img className="square" alt="loading" src={obj.titleSrc} />
-                </li>
-              );
-            })
-          ) : (
-            <img
-              className="bookBatchPills"
-              src="https://as1.ftcdn.net/jpg/02/01/18/68/500_F_201186834_NLRQImmmfLmOHEQpQPXB6wh4F4quHloV.jpg"
-              alt="loading"
-            />
-          )}
+          {FeaturedSeries.map((obj) => (
+            <li key={obj.titleSrc} className="list list-group-item">
+              <img className="square" alt="loading" src={obj.titleSrc} />
+            </li>
+          ))}
         </ul>
       </div>
     </>
