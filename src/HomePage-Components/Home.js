39ec19118d8eb23f4eb40bbb7f9863a72683a99c
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2021-08-30 23:16:40 +0530

    added features in alice carousel

--- src/HomePage-Components/Home.js
+++ src/HomePage-Components/Home.js
@@ -3,25 +3,33 @@ import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 export function Home() {
+  const items = [
+    <img
+      alt=""
+      className="ImgHomePage"
+      src="https://www.detroitlabs.com/wp-content/uploads/2018/02/alfons-morales-YLSwjSy7stw-unsplash.jpg"
+    />,
+    <img
+      alt=""
+      className="ImgHomePage"
+      src="https://whataftercollege.com/wp-content/uploads/2019/03/3-2-1024x535.jpg"
+    />,
+    <img
+      alt=""
+      className="ImgHomePage"
+      src="https://static.onecms.io/wp-content/uploads/sites/34/2019/12/bookshelf-organization-dark-room-full-rick-lozier-1219.jpg"
+    />
+  ];
   return (
     <>
-      <AliceCarousel autoPlay autoPlayInterval="1000">
-        <img
-          src="https://www.detroitlabs.com/wp-content/uploads/2018/02/alfons-morales-YLSwjSy7stw-unsplash.jpg"
-          className="ImgHomePage"
-          alt=""
-        />
-        <img
-          src="https://whataftercollege.com/wp-content/uploads/2019/03/3-2-1024x535.jpg"
-          className="ImgHomePage"
-          alt=""
-        />
-        <img
-          src="https://static.onecms.io/wp-content/uploads/sites/34/2019/12/bookshelf-organization-dark-room-full-rick-lozier-1219.jpg"
-          alt=""
-          className="ImgHomePage"
-        />
-      </AliceCarousel>
+      <AliceCarousel
+        mouseTracking
+        autoPlay
+        autoPlayInterval="1500"
+        infinite
+        disableButtonsControls
+        items={items}
+      />
 
       <useComponent.FeaturedAuthors />
       <useComponent.BookBatches />
